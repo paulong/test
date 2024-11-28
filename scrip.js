0527bf55cc852e328e2222f9861cc8c1068f0183
Paulo <aenemapro@gmail.com>
2024-11-28 16:46:38 -0400

    first commit

---
+++ scrip.js
@@ -0,0 +1,22 @@
+<div></div>
+<script type="text/javascript">
+  (function(d, t) {
+    var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    v.onload = function() {
+      if (!document.getElementById('root')) {
+        var root = d.createElement('div');
+        root.id = 'root';
+        d.body.appendChild(root);
+      }
+      if (window.myChatWidget && typeof window.myChatWidget.load === 'function') {
+        window.myChatWidget.load({
+          id: 'd014843a-ea0b-436a-b511-660c40bc5de5',
+        });
+      }
+    };
+    v.src = "https://agentivehub.com/production.bundle.min.js";
+    v.type = "text/javascript";
+    s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
+</div>
